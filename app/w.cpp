8d28e77417eddadd93115e79714e9328cec66c62
Stasno <54759371+Stasno@users.noreply.github.com>
2021-11-06 16:25:45 +0700

    Initial commit

---
+++ app/w.cpp
@@ -0,0 +1,5 @@
+//
+// Created by stas on 05.11.2021.
+//
+
+#include "w.h"
---
+++ app/w.h
@@ -0,0 +1,16 @@
+//
+// Created by stas on 05.11.2021.
+//
+
+#ifndef LAB8_W_H
+#define LAB8_W_H
+
+
+
+class w {
+
+};
+
+
+
+#endif //LAB8_W_H
